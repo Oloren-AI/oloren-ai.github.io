73a21f9c71e06849da76b2cd7bf4d300aec461bc
du21m <melissa.duyuan@gmail.com>
2022-06-28 16:06:49 +0000

    blog

--- blog/server.js
+++ blog/server.js
@@ -4,12 +4,16 @@ const path = require('path');
 let init_path = __dirname;
 
 const app = express();
+
+hljs = require('highlight.js');
+html = hljs.highlightAuto('<h1>Hello World!</h1>').value
+
 app.use(express.static(init_path));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(init_path, "home.html"));
 })
 
-app.listen("3000", () => {
+app.listen("3010", () => {
     console.log("listening...");
 })

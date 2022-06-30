4f78a041483659d5ad99aeeaf0279439564eb39e
David Huang <david.zq.huang@gmail.com>
2022-06-29 21:27:31 -0500

    linked up + first stab at landing + blobs

--- js/main.js
+++ js/main.js
@@ -53,6 +53,7 @@ var blobs = document.getElementsByClassName('blob');
 for (var i = blobs.length-1; i >= 0; --i) {
     var blob = blobs[i];  
     console.log("BLOB")
+    console.log(blob.getAttribute('fill'))
     const svgPath = blobs2.svgPath({
       seed: Math.random(),
       extraPoints: 8,
@@ -62,10 +63,36 @@ for (var i = blobs.length-1; i >= 0; --i) {
     var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     var path1 = document.createElementNS("http://www.w3.org/2000/svg", 'path');
     path1.setAttribute('d', svgPath);
+    path1.setAttribute('fill', blob.getAttribute('fill'))
+    var path2 = document.createElementNS("http://www.w3.org/2000/svg", 'path');
+    path2.setAttribute('d', svgPath);
+    path2.setAttribute('transform', 'scale(0.9375,0.9375)translate(8 8)')
+    path2.setAttribute('fill', blob.getAttribute('fill2'))
+    var title = document.createElementNS("http://www.w3.org/2000/svg", 'text');
+    title.textContent = blob.getAttribute("title")
+    title.setAttribute('x', '50%');
+    title.setAttribute('y', '50%');
+    title.setAttribute('text-anchor', 'middle')
+    var description = document.createElementNS("http://www.w3.org/2000/svg", 'text');
+    description.textContent = blob.getAttribute("description")
+    description.setAttribute('width', '30%')
+    description.setAttribute('x', '50%');
+    description.setAttribute('y', '60%');
+    description.setAttribute('text-anchor', 'middle')
     svg.appendChild(path1);
-    svg.setAttribute('width', 256);
-    svg.setAttribute('height', 256);
-    blob.appendChild(svg);
+    svg.appendChild(path2);
+    svg.appendChild(title);
+    svg.appendChild(description)
+    svg.setAttribute('width', '100%');
+    svg.setAttribute('viewBox', '0 0 256 256');
+    svg.setAttribute('id', `blogblob${i}`);
+    var s = new XMLSerializer().serializeToString(svg)
+    var encodedData = window.btoa(s);
+    console.log(`data:image/svg+xml;base64,${encodedData}`)
+    blob.style.backgroundImage = `url(data:image/svg+xml;base64,${encodedData})`;
+    blob.style.backgroundRepeat = 'no-repeat';
+    svg.setAttribute('visibility', 'hidden');
+
 }
 
 
